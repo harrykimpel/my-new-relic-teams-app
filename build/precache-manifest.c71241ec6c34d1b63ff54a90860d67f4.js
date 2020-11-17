12cf2fb854bdf079ae3b65a73b983eb8d0e8003d
Harry Kimpel <hkimpel@newrelic.com>
2020-11-17 10:31:49 +0100

    initial commit

---
+++ build/precache-manifest.c71241ec6c34d1b63ff54a90860d67f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0519874971449f3402727d283f775bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b7ad95a9548f25cbb04",
+    "url": "/static/css/main.4a1b6a88.chunk.css"
+  },
+  {
+    "revision": "1e7576ddb9de037695a4",
+    "url": "/static/js/2.1fee81b5.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/static/js/2.1fee81b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7ad95a9548f25cbb04",
+    "url": "/static/js/main.296a60d6.chunk.js"
+  },
+  {
+    "revision": "9bfd6d225e4168839554",
+    "url": "/static/js/runtime-main.22145939.js"
+  }
+]);
